9a3d659be6a26335f26498c14df03cb24a55b3c9
gabrielabud <gabriela.budeanu@gmail.com>
2017-11-27 15:26:19 +0000

    Add book-link option and feature test passing

--- spec/features/user_choice_spec.rb
+++ spec/features/user_choice_spec.rb
@@ -4,9 +4,17 @@ feature 'Allowing user to choose to list or book' do
     expect(page).to have_button('List')
     expect(page).to have_button('Book')
   end
-  scenario 'Choosing to list' do
+  
+  scenario 'Choosing to list takes the user to a page for listing a new space' do
     visit '/'
     click_link('list-link')
     expect(page.current_path).to eq('/spaces/new')
   end
+
+  scenario 'Choosing to book takes the user to spaces page' do
+    visit '/'
+    click_link('book-link')
+    expect(page.current_path).to eq('/spaces')
+  end
+
 end

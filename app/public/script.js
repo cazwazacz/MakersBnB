2a761f5c83eff59cd7333aeab06b7f7409e5149a
antonio <antoniobelmardacosta@gmail.com>
2017-11-30 12:23:28 +0000

    Refactor jQuery to include displaySpaces function

--- app/public/script.js
+++ app/public/script.js
@@ -5,14 +5,7 @@ $(document).ready(function() {
       $("#info").empty();
       $.get('/api/spaces', function(information) {
         for(var i=0; i< information.info.length; i++) {
-          $("#info").append('<div id=space-' + information.info[i].id + '>' + information.info[i].title + information.info[i].description + information.info[i].price + information.info[i].location + '</div>')
-          $("#space-" + information.info[i].id).append('<img src=' + information.photo[i].image.url + '>');
-          if(information.info[i].available) {
-            $("#space-" + information.info[i].id).append('<button class="book-button" id=book-button-' + information.info[i].id + ' type="button"> Book </button>')
-          }
-          else {
-            $("#space-" + information.info[i].id).append(' Unavailable')
-          }
+          displaySpaces(information, i);
         }
       })
     } else if(filterPrice === "" || filterPrice=== undefined){
@@ -20,14 +13,7 @@ $(document).ready(function() {
       $.get('/api/spaces', function(information) {
         for(var i=0; i< information.info.length; i++) {
           if(information.info[i].location === filterLocation) {
-            $("#info").append('<div id=space-' + information.info[i].id + '>' + information.info[i].title + information.info[i].description + information.info[i].price + information.info[i].location + '</div>')
-            $("#space-" + information.info[i].id).append('<img src=' + information.photo[i].image.url + '>');
-            if(information.info[i].available) {
-              $("#space-" + information.info[i].id).append('<button class="book-button" id=book-button-' + information.info[i].id + ' type="button"> Book </button>')
-            }
-            else {
-              $("#space-" + information.info[i].id).append(' Unavailable')
-            }
+            displaySpaces(information, i);
           }
         }
       })
@@ -36,14 +22,7 @@ $(document).ready(function() {
       $.get('/api/spaces', function(information) {
         for(var i=0; i< information.info.length; i++) {
           if(information.info[i].price <= filterPrice){
-            $("#info").append('<div id=space-' + information.info[i].id + '>' + information.info[i].title + information.info[i].description + information.info[i].price + information.info[i].location + '</div>')
-            $("#space-" + information.info[i].id).append('<img src=' + information.photo[i].image.url + '>');
-            if(information.info[i].available) {
-              $("#space-" + information.info[i].id).append('<button class="book-button" id=book-button-' + information.info[i].id + ' type="button"> Book </button>')
-            }
-            else {
-              $("#space-" + information.info[i].id).append(' Unavailable')
-            }
+            displaySpaces(information, i);
           }
         }
       })
@@ -52,19 +31,22 @@ $(document).ready(function() {
       $.get('/api/spaces', function(information) {
         for(var i=0; i< information.info.length; i++) {
           if(information.info[i].price <= filterPrice && information.info[i].location === filterLocation) {
-            $("#info").append('<div id=space-' + information.info[i].id + '>' + information.info[i].title + information.info[i].description + information.info[i].price + information.info[i].location + '</div>')
-            $("#space-" + information.info[i].id).append('<img src=' + information.photo[i].image.url + '>');
-            if(information.info[i].available) {
-              $("#space-" + information.info[i].id).append('<button class="book-button" id=book-button-' + information.info[i].id + ' type="button"> Book </button>')
-            }
-            else {
-              $("#space-" + information.info[i].id).append(' Unavailable')
-            }
+            displaySpaces(information, i);
           }
         }
       })
     }
   };
+
+  displaySpaces = function(information, i) {
+    $("#info").append('<div id=space-' + information.info[i].id + '>' + information.info[i].title + information.info[i].description + information.info[i].price + information.info[i].location + '</div>')
+    $("#space-" + information.info[i].id).append('<img src=' + information.photo[i].image.url + '>');
+    if(information.info[i].available) {
+      $("#space-" + information.info[i].id).append('<button class="book-button" id=book-button-' + information.info[i].id + ' type="button"> Book </button>')
+    } else {
+      $("#space-" + information.info[i].id).append(' Unavailable')
+    }
+  }
 
   getSpaces();
 
